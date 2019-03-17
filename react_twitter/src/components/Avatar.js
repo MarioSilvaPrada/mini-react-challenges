74eb4f715006e71d0f57dd2592cefc0b9075d79f
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-03-17 18:13:12 +0000

    updating react twitter

---
+++ react_twitter/src/components/Avatar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Avatar = () => {
+    return (
+        <img
+            src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+            className="avatar"
+            alt="avatar" />
+    )
+}
+
+export default Avatar;
